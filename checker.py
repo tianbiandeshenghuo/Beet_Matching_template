939f7a9f4c3052b929967f97c28f467e4d936618
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2024-02-18 16:30:55 +0800

    Update checker.py

--- checker.py
+++ checker.py
@@ -2,6 +2,7 @@ from cyaron import * #改用 luogu-dev 提供的 cyaron 生成数据
 from random import *
 # 如果您没有下载，请在终端使用 pip install cyaron 直接安装
 # 如果还没有下载 python，请自行安装
+# cyaron 的使用请参考 https://github.com/luogu-dev/cyaron/wiki
 a = randint(1, int(1e9))
 b = randint(1, int(1e9))
 print(a, b)
